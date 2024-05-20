cda011a48adae15a84347f9f3ee9d00cff338518
yujiee999 <166993066+yujiee999@users.noreply.github.com>
2024-05-20 09:38:41 +0800

    Update 1.ST_Var_BRCA_MutSitesCountCluster.R

--- code/1.ST_Var_BRCA_MutSitesCountCluster.R
+++ code/1.ST_Var_BRCA_MutSitesCountCluster.R
@@ -58,6 +58,7 @@ spot_gene_mutcount<-data.frame()
 for (file in file_list) {
   df2<- read.table(file, header=T,sep="\t",as.is=T,comment.char = "")
   df2 <- df2[which(df2$ALT_expected!="."),]
+  df2 <- df2[which(df2$Base_observed!=df2$REF),]##保留SNV所在行，即观测碱基不等于参考碱基的行
   colnames(df2)[1]<-"Chr"
   df2<-merge(df2,anno2)
   ##spot的基因突变次数
