8886037374bece226bec82c776253b818d2ecd36
yujiee999 <166993066+yujiee999@users.noreply.github.com>
2024-05-22 10:36:26 +0800

    Update 1.ST_Var_BRCA_MutSitesCountCluster.R

--- code/1.ST_Var_BRCA_MutSitesCountCluster.R
+++ code/1.ST_Var_BRCA_MutSitesCountCluster.R
@@ -1,3 +1,11 @@
+#更改输入输出文件夹名
+disease <- "BRCA"
+input <- paste0("J:/database/STMut/data_process/",disease)
+output_inter <- paste0(""J:/database/STMut/data_process/inter/",disease)
+output <- paste0(""J:/database/STMut/data_process/result/",disease)
+output_table <- paste0(""J:/database/STMut/data_process/result_table/",disease)
+GSE_id <- "GSE203612"
+SAM_id <- "GSM6177599"
 #载入所需的R包；
 library(Seurat)#
 library(ggplot2)
@@ -5,7 +13,7 @@ library(patchwork)#
 library(dplyr)
 library(hdf5r) #
 ###############################读取空转数据################################filtered_feature_bc_matrix.h5文件和spatial文件夹
-data_dir<- "E:/数据库/STMut/数据处理/BRCA/GSE203612/GSM6177599"
+data_dir<- paste(input,GSE_id,SAM_id,sep="/")
 list.files(data_dir) #查看目录中的文件；
 file_name<- "filtered_feature_bc_matrix.h5"
 brain<- Load10X_Spatial(data.dir = data_dir, filename = file_name,slice = "anterior1")
@@ -35,16 +43,16 @@ p10 <- SpatialDimPlot(brain, label = TRUE, pt.size.factor = 3,label.size = 12,al
 options(repr.plot.width=16, repr.plot.height=8)#让图片展示变大
 SpatialDimPlot(brain,alpha =0) + NoLegend() + p9 + p10
 
-saveRDS(brain, "E:/数据库/STMut/数据处理/中间文件/ST_seuratObject.rds")
+saveRDS(brain, paste(output_inter,GSE_id,SAM_id,"ST_seuratObject.rds",sep="/"))
 ###########################################################################
 ##########################先跑出p10以前的代码，然后##########################
 ###########################################################################
 ##########################基因突变位点数突变谱聚类###############################
 ############################################################################
 #参考 GSE167036_Cell_subsets_definedby_mutGenes.R
-file_path <- "E:/数据库/STMut/数据处理/BRCA/GSE203612/GSM6177599/Step4_VariantCalling"
+file_path <- paste(input,GSE_id,SAM_id,"Step4_VariantCalling",sep="/")
 ##########################0-读取scsnv注释矩阵###############################后缀hg38_multianno.csv的文件
-anno<- read.table(paste0(file_path,"/GSM6177599.variants.annovar.Step4.3.1.hg38_multianno.csv"), header=T,sep=",",as.is=T,fill=T)
+anno<- read.table(paste0(file_path,"/",SAM_id,"variants.annovar_all.hg38_multianno.csv"), header=T,sep=",",as.is=T,fill=T)
 anno<-anno[,c(1,2,7)]
 anno<- anno[nchar(anno[,3]) > 1,]# 保留第三列字符长度大于1的行
 anno<-anno[!grepl("\\|", anno[,3]), ]
@@ -79,11 +87,42 @@ spot_gene_mutcount_matrix <- merge(data.frame(CB = rownames(brain@meta.data)), s
 rownames(spot_gene_mutcount_matrix) <- spot_gene_mutcount_matrix[,1]
 spot_gene_mutcount_matrix[is.na(spot_gene_mutcount_matrix)] <- 0
 
-write.table(spot_gene_mutcount_matrix, "E:/数据库/STMut/数据处理/中间文件/spot_gene_mutcount_matrix.txt", sep = "\t", quote = F)
+write.table(spot_gene_mutcount_matrix, paste(output_inter,GSE_id,SAM_id,"spot_gene_mutcount_matrix.txt",sep="/") sep = "\t", quote = F)
+
+##质量控制
+###不过滤变异的细胞数目非常少的基因
+#gene_count <- rowSums(spot_gene_mutcount_matrix[, -1] > 0)# 计算每个基因在多少个细胞中表达
+#filtered_gene_data <- spot_gene_mutcount_matrix[gene_count >= 100, ]# 筛选出在100个或更多细胞中表达的基因
+seurat_object <- CreateSeuratObject(counts = t(spot_gene_mutcount_matrix[,-1]))
+VlnPlot(object = seurat_object, features = c("nCount_RNA", "nFeature_RNA"))
+#不打算过滤变异数目非常多或者非常少的细胞,因为表达过滤了低质量的细胞
+#seurat_object <- subset(seurat_object,subset=nFeature_RNA>10 & nFeature_RNA<300 )
+# 可选：进行预处理和标准化
+seurat_object <- NormalizeData(seurat_object, normalization.method = 'LogNormalize', scale.factor = 100)
+seurat_object <- FindVariableFeatures(seurat_object, selection.method = "vst", nfeatures = 2000)
+top10 <- head(VariableFeatures(seurat_object),10)#画出不带标签或带标签高变突变基因点图
+plot1 <- VariableFeaturePlot(seurat_object)
+top10
+plot2 <- LabelPoints(plot = plot1,points = top10,repel = TRUE)
+plot2
+seurat_object <- ScaleData(seurat_object, features = rownames(seurat_object))
+##########################3-scsnv突变矩阵降维聚类###############################
+#6. 降维聚类
+#（基于前面得到的high variable基因的scale矩阵）
+seurat_object <- RunPCA(seurat_object, npcs = 50, verbose = FALSE)
+seurat_object <- FindNeighbors(seurat_object, dims = 1:30)
+seurat_object <- FindClusters(seurat_object, resolution = 0.5)
+
+seurat_object <- RunUMAP(seurat_object, dims = 1:30)
+#seurat_object <- RunTSNE(seurat_object, dims = 1:30)
+DimPlot(seurat_object , reduction = "umap", pt.size=0.5)
+
+#输出位点数突变谱的seurat对象
+saveRDS(seurat_object,  paste(output_inter,GSE_id,SAM_id,"MutSitesCount_seuratObject.rds",sep="/"))
 
 spot_gene_mutcount_matrix[, -1][spot_gene_mutcount_matrix[, -1] > 0] <- 1
 
-write.table(spot_gene_mutcount_matrix, "E:/数据库/STMut/数据处理/中间文件/spot_gene_01_matrix.txt", sep = "\t", quote = F)
+write.table(spot_gene_mutcount_matrix, paste(output_inter,GSE_id,SAM_id,"spot_gene_01_matrix.txt",sep="/"), sep = "\t", quote = F)
 ##质量控制
 ###不过滤变异的细胞数目非常少的基因
 #gene_count <- rowSums(spot_gene_mutcount_matrix[, -1] > 0)# 计算每个基因在多少个细胞中表达
@@ -113,18 +152,15 @@ seurat_object <- RunUMAP(seurat_object, dims = 1:30)
 DimPlot(seurat_object , reduction = "umap", pt.size=0.5)
 
 #输出01突变谱的seurat对象
-saveRDS(seurat_object, "E:/数据库/STMut/数据处理/中间文件/Mut01_seuratObject.rds")
-
-#输出位点数突变谱的seurat对象
-saveRDS(seurat_object, "E:/数据库/STMut/数据处理/中间文件/MutSitesCount_seuratObject.rds")
+saveRDS(seurat_object, paste(output_inter,GSE_id,SAM_id,"Mut01_seuratObject.rds",sep="/"))
 
 
 ##########################4-空转展示scsnv突变聚类类别###############################
 #***********************************************************************************
 #
-seurat_object_1 <- readRDS("E:/数据库/STMut/数据处理/中间文件/Mut01_seuratObject.rds")
-seurat_object_2 <- readRDS("E:/数据库/STMut/数据处理/中间文件/MutSitesCount_seuratObject.rds")
-brain <- readRDS("E:/数据库/STMut/数据处理/中间文件/ST_seuratObject.rds")
+seurat_object_1 <- readRDS(paste(output_inter,GSE_id,SAM_id,"Mut01_seuratObject.rds",sep="/"))
+seurat_object_2 <- readRDS(paste(output_inter,GSE_id,SAM_id,"MutSitesCount_seuratObject.rds",sep="/"))
+brain <- readRDS(paste(output_inter,GSE_id,SAM_id,"ST_seuratObject.rds",sep="/"))
 
 head(seurat_object_1@meta.data)
 head(seurat_object_2@meta.data)
@@ -146,7 +182,7 @@ brain@meta.data <- cbind(brain@meta.data, mut_cluster[,-1])
 nCount_nFeatureSNV <- data.frame(CB = rownames(seurat_object_2@meta.data), seurat_object_2@meta.data[,c(2,3)])
 brain@meta.data <- cbind(brain@meta.data, nCount_nFeatureSNV[,-1])
 
-saveRDS(brain, "E:/数据库/STMut/数据处理/中间文件/ST_Mut_SeuratObject.rds")
+saveRDS(brain, paste(output_inter,GSE_id,SAM_id,"ST_Mut_SeuratObject.rds",sep="/"))
 
 SpatialDimPlot(brain, group.by="mut_cluster",label = TRUE, pt.size.factor = 3,label.size = 12,alpha = c(0.8, 1),image.alpha = 0)+ 
   theme(text = element_text(size=24))
@@ -171,7 +207,7 @@ SpatialDimPlot(brain, group.by="mut_cluster",label = TRUE, pt.size.factor = 3,la
 # SpatialFeaturePlot(brain, features = top10)#展示空转基因突变计数
 
 #********************************************************
-brain <- readRDS("E:/数据库/STMut/数据处理/中间文件/ST_Mut_SeuratObject.rds")
+brain <- readRDS(paste(output_inter,GSE_id,SAM_id,"ST_Mut_SeuratObject.rds",sep="/"))
 
 #111111这里导出小提琴图，放到数据库里面
 nCount_Spatial <- VlnPlot(object = brain, features = c("nCount_Spatial"), group.by = "orig.ident")+theme(legend.position = "none",axis.text.x = element_blank())
@@ -180,7 +216,7 @@ nCount_Spatial <- VlnPlot(object = brain, features = c("nCount_Spatial"), group.
 # # 进行绘图操作
 # dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/nCount_Spatial_viloin_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"nCount_Spatial_viloin_blow.png",sep="/"), width = 674, height = 674)
 VlnPlot(object = brain, features = c("nCount_Spatial"), group.by = "orig.ident")+theme(legend.position = "none",axis.text.x = element_blank())
 dev.off()
 
@@ -190,18 +226,18 @@ nFeature_Spatial <- VlnPlot(object = brain, features = c("nFeature_Spatial"), gr
 # # 进行绘图操作
 # dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/nFeature_Spatial_viloin_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"nFeature_Spatial_viloin_blow.png",sep="/"), width = 674, height = 674)
 VlnPlot(object = brain, features = c("nFeature_Spatial"), group.by = "orig.ident")+theme(legend.position = "none",axis.text.x = element_blank())
 dev.off()
 
 #这里导出小提琴图，放到数据库里面
 nCount_RNA <- VlnPlot(object = brain, features = c("nCount_RNA"), group.by = "orig.ident")+theme(legend.position = "none",axis.text.x = element_blank())+labs(title="nCount_SNV")
-# png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/nCount_RNA_viloin.png", width = 337, height = 280)
+# png(paste(output,GSE_id,SAM_id,"nCount_RNA_viloin.png",sep="/"), width = 337, height = 280)
 # print(nCount_RNA)
 # # 进行绘图操作
 # dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/nCount_SNV_viloin_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"nCount_SNV_viloin_blow.png",sep="/"), width = 674, height = 674)
 print(nCount_RNA)
 # 进行绘图操作
 dev.off()
@@ -212,7 +248,7 @@ nFeature_RNA <- VlnPlot(object = brain, features = c("nFeature_RNA"), group.by =
 # # 进行绘图操作
 # dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/nFeature_SNV_viloin_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"nFeature_SNV_viloin_blow.png",sep="/"), width = 674, height = 674)
 print(nFeature_RNA)
 # 进行绘图操作
 dev.off()
@@ -226,7 +262,7 @@ nCount_RNA <- SpatialFeaturePlot(brain, features="nCount_RNA",pt.size.factor = 3
 # # 进行绘图操作
 # dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/nCount_SNV_slice_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"nCount_SNV_slice_blow.png",sep="/"), width = 674, height = 674)
 print(nCount_RNA)
 # 进行绘图操作
 dev.off()
@@ -239,7 +275,7 @@ nFeature_RNA <- SpatialFeaturePlot(brain, features="nFeature_RNA",pt.size.factor
 # # 进行绘图操作
 # dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/nFeature_SNV_slice_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"nFeature_SNV_slice_blow.png",sep="/"), width = 674, height = 674)
 print(nFeature_RNA)
 # 进行绘图操作
 dev.off()
@@ -251,7 +287,7 @@ nCount_Spatial <- SpatialFeaturePlot(brain, features="nCount_Spatial",pt.size.fa
 # # 进行绘图操作
 # dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/nCount_Spatial_slice_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"nCount_Spatial_slice_blow.png",sep="/"), width = 674, height = 674)
 print(nCount_Spatial)
 # 进行绘图操作
 dev.off()
@@ -263,18 +299,18 @@ nFeature_Spatial <- SpatialFeaturePlot(brain, features="nFeature_Spatial",pt.siz
 # # 进行绘图操作
 # dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/nFeature_Spatial_slice_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"nFeature_Spatial_slice_blow.png",sep="/"),width = 674, height = 674)
 print(nFeature_Spatial)
 # 进行绘图操作
 dev.off()
 
 #这里导出cluster图---------------------------------------------------------------------------------------------------------------------
 seurat_object <- seurat_object_1
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/cluster_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"cluster_blow.png",sep="/"), width = 674, height = 674)
 DimPlot(seurat_object, reduction = "umap", pt.size=0.5)
 dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/cluster_slice_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"cluster_slice_blow.png",sep="/"), width = 674, height = 674)
 SpatialDimPlot(brain, group.by="mut_cluster",label = TRUE, pt.size.factor = 3,label.size = 12,alpha = c(0.8, 1),image.alpha = 1)+ 
   theme(text = element_text(size=24))
 dev.off()
@@ -293,7 +329,7 @@ Slice_Coor_Cluster_integrate <- cbind(Slice_Coor_cluster, spot_corrdinates)
 
 #突变谱
 library(tidyverse)
-spot_gene_mutcount_matrix <- read.table("E:/数据库/STMut/数据处理/中间文件/spot_gene_mutcount_matrix.txt", sep = "\t", row.names = 1)
+spot_gene_mutcount_matrix <- read.table(paste(output_inter,GSE_id,SAM_id,"spot_gene_mutcount_matrix.txt",sep="/"), sep = "\t", row.names = 1)
 long_data <- spot_gene_mutcount_matrix %>% 
              gather(key = "Gene", value = "MutCount", -CB)
 
@@ -346,7 +382,7 @@ long_data_5 <- long_data_4[,c(1,4)]
 colnames(long_data_5) <- c("Column_name", "Column_value")
 
 result_4 <- rbind(result_3, long_data_5)
-write.table(result_4, "E:/数据库/STMut/数据处理/结果表格/Slice_Coor_Cluster_Mut.txt", row.names = F, quote = F, sep = "\t")
+write.table(result_4, paste(output_table,GSE_id,SAM_id,Slice_Coor_Cluster_Mut.txt,sep="/"), row.names = F, quote = F, sep = "\t")
 #********************************************************
 ###########################################################################
 ##########################先跑出p10以前的代码，然后##########################
@@ -454,14 +490,14 @@ seurat_object@meta.data <- cbind(seurat_object@meta.data, mut_cluster[,-1])
 View(seurat_object@meta.data)
 View(seurat_object_2@meta.data)
 
-saveRDS(seurat_object, "E:/数据库/STMut/数据处理/中间文件/MutSites_01_SeuratObject.rds")
+saveRDS(seurat_object, paste(output_inter,GSE_id,SAM_id,"MutSites_01_SeuratObject.rds",sep="/"))
 
 seurat_object.markers <- FindAllMarkers(seurat_object, only.pos = TRUE, min.pct = 0.25)
 seurat_object.markers[,c(1,2,5)] <- round(seurat_object.markers[,c(1,2,5)], 4)
-seurat_object.markers$disease <- "BRCA"
-seurat_object.markers$dataset <- "GSE203612"
-seurat_object.markers$sample <- "GSM6177599"
-write.table(seurat_object.markers, "E:/数据库/STMut/数据处理/结果表格/BRCA_GSE203612_markers.txt", sep = "\t", row.names = F, quote = F)
+seurat_object.markers$disease <- disease
+seurat_object.markers$dataset <- GSE_id 
+seurat_object.markers$sample <- SAM_id
+write.table(seurat_object.markers, paste(output_table,GSE_id,SAM_id,disease,"_",GSE_id,"_markers.txt",sep="/"), sep = "\t", row.names = F, quote = F)
 #查看每个亚群的前两个
 library(tidyverse)
 seurat_object.markers %>% 
@@ -471,22 +507,22 @@ seurat_object.markers %>%
 top5 <- seurat_object.markers %>% 
   group_by(cluster) %>%
   top_n(n = 5, wt = avg_log2FC)
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/top5marker_heatmap.png", width = 337, height = 280)
+png(paste(output,GSE_id,SAM_id,"top5marker_heatmap.png",sep="/"), width = 337, height = 280)
 DoHeatmap(seurat_object, features = top5$gene) + NoLegend()
 dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/top5marker_heatmap_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"top5marker_heatmap_blow.png",sep="/"), width = 674, height = 674)
 DoHeatmap(seurat_object, features = top5$gene) + NoLegend()
 dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/top5marker_dotplot.png", width = 337, height = 280)
+png(paste(output,GSE_id,SAM_id,"top5marker_dotplot.png",sep="/"), width = 337, height = 280)
 DotPlot(seurat_object, features = unique(top5$gene), scale = F) + RotatedAxis() + guides(shape=guide_legend(title = NULL),
                                                                                          color=guide_legend(title = 'Average Mutation'),
                                                                                          size=guide_legend(title = 'Percent Mutated'),
 )
 dev.off()
 
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/top5marker_dotplot_blow.png", width = 674, height = 674)
+png(paste(output,GSE_id,SAM_id,"top5marker_dotplot_blow.png",sep="/"), width = 674, height = 674)
 DotPlot(seurat_object, features = unique(top5$gene), scale = F) + RotatedAxis() + guides(shape=guide_legend(title = NULL),
                                                                                          color=guide_legend(title = 'Average Mutation'),
                                                                                          size=guide_legend(title = 'Percent Mutated'),
@@ -510,7 +546,7 @@ erich.go.BP = enrichGO(gene = x1,
                        ont = "BP",
                        pvalueCutoff = 0.05,
                        qvalueCutoff = 0.05)
-png("E:/数据库/STMut/数据处理/结果/BRCA/GSE203612/GSM6177599/GO_enrichment.png", width = 1200, height = 674)
+png(paste(output,GSE_id,SAM_id,"GO_enrichment.png",sep="/"), width = 1200, height = 674)
 dotplot(erich.go.BP, showCategory = 10)
 dev.off()
 
@@ -522,7 +558,7 @@ dev.off()
 #                        qvalueCutoff = 0.05)
 # dotplot(erich.go.MF, showCategory = 10)
 
-write.csv(go,file="E:/数据库/STMut/数据处理/结果表格/go_0.05_ENTREZID_0.02.csv")
+write.csv(go,file=paste(output_table,GSE_id,SAM_id,"go_0.05_ENTREZID_0.02.csv",sep="/"))
 
 #KEGG
 #富集没有结果
